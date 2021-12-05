251d002e112406a477031dd017f36fe3f29f9837
Zhiyuan Cui <zhicui@cisco.com>
2021-12-05 21:26:40 +0800

    first cpp program

---
+++ chapter01.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string _name;
+    cout << "请输入你的名字：";
+    cin >> _name;
+
+    cout << "Hello, "
+         << _name;
+}
