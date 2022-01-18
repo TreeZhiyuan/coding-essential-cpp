a44c5760a832cd58c2635b243dc6897a2b3876d2
Zhiyuan Cui <zhicui@cisco.com>
2022-01-18 16:48:19 +0800

    第一章

--- chapter01.cpp
+++ chapter01.cpp
@@ -3,10 +3,16 @@
 using namespace std;
 int main()
 {
-    string _name;
-    cout << "请输入你的名字：";
-    cin >> _name;
-
-    cout << "Hello, "
-         << _name;
+     string _firstname, _lastname;
+     int k = 0;
+     int i = 0;
+     cout << "please input your firstname: ";
+     cin >> _firstname;
+     cout << "\r\n";
+     cout << "please input your lastname: ";
+     cin >> _lastname;
+     cout << "\r\n";
+     cout << "hello " << _lastname << " " << _firstname << "\r\n";
+     cout << "k++" << k++ << "\n\r";
+     cout << "++i" << ++i << "\n\r";
 }
